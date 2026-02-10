f12897a2bb1439e09c4215bccde13f8d6a5ca11a
manupereiraduarte <98099@sistemas.frc.utn.edu.ar>
2026-02-10 20:27:29 +0000

    dice game starter

---
+++ TurbineB_Q126_manupereiraduarte/anchor-dice-game-q1-26/programs/anchor-dice-game-q1-26/src/instructions/resolve_bet.rs
@@ -0,0 +1,52 @@
+use anchor_lang::{prelude::*, system_program::{transfer, Transfer}};
+use anchor_instruction_sysvar::Ed25519InstructionSignatures;
+use solana_program::{sysvar::instructions::load_instruction_at_checked, ed25519_program, hash::hash};
+
+use crate::{state::Bet, errors::DiceError};
+
+pub const HOUSE_EDGE: u64 = 150;
+
+#[derive(Accounts)]
+pub struct ResolveBet<'info> {
+    #[account(mut)]
+    pub house: Signer<'info>,
+
+    #[account(mut)]
+    pub player: UncheckedAccount<'info>,
+
+    #[account(
+        mut,
+        seeds = [b"vault", house.key().as_ref()],
+        bump
+    )]
+    pub vault: SystemAccount<'info>,
+
+    #[account(
+        mut,
+        has_one = player,
+        close = player,
+        seeds = [b"bet", vault.key().as_ref(), bet.seed.to_le_bytes().as_ref()],
+        bump = bet.bump
+    )]
+    pub bet: Account<'info, Bet>,
+
+    #[account(
+        address = solana_program::sysvar::instructions::ID
+    )]
+    pub instruction_sysvar: UncheckedAccount<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+impl<'info> ResolveBet<'info> {
+    pub fn verify_ed25519_signature(&mut self, sig: &[u8]) -> Result<()> {
+
+        Ok(())
+    }
+
+    pub fn resolve_bet(&mut self, bumps: &ResolveBetBumps, sig: &[u8]) -> Result<()> {
+
+
+        Ok(())
+    }
+}
